cb772377de02c50651c0c0529130f4d8f288e001
Mitalee <raomitalee@gmail.com>
2018-05-24 19:43:33 +0530

    Added functions for creating xml and sending a tally import request for one data row.

--- csv_to_tally.py
+++ csv_to_tally.py
@@ -11,10 +11,99 @@ class TallyImporter:
 		self.filename = filename
 		if self.filename:
 			self.df = pd.read_csv(self.filename, header=0, index_col=False)
-			print('loaded file into df.')
+			print('loaded file into a dataframe "df".')
 		
 
 	def clean_columns(self):
 		self.df.columns = [c.lower().replace(' ', '_').replace('/','_') for c in self.df.columns]
 		self.df['invoice_date'] = pd.to_datetime(self.df['invoice_date']).dt.strftime('%Y%m%d')
 		print(self.df.columns)
+
+	def create_xml(self, row):
+		xml_op = """<ENVELOPE>
+				<HEADER>
+					<VERSION>1</VERSION>
+					<TALLYREQUEST>Import</TALLYREQUEST>
+					<TYPE>Data</TYPE>
+					<ID>Vouchers</ID>
+				</HEADER>
+				<BODY>
+					<DESC>
+						<STATICVARIABLES>
+							<IMPORTDUPS>@@DUPIGNORE</IMPORTDUPS>
+						</STATICVARIABLES>
+					</DESC>
+				<DATA>\n"""
+
+		xml_op += f"""<TALLYMESSAGE>
+				<VOUCHER VCHTYPE='Sales' ACTION='Create'>
+					<DATE>{row.invoice_date}</DATE> 
+					<NARRATION> Amazon Order ID: {row.order_id}</NARRATION>
+					<VOUCHERTYPENAME>Sales</VOUCHERTYPENAME>
+					<LEDGERENTRIES.LIST>
+						<ISDEEMEDPOSITIVE>Yes</ISDEEMEDPOSITIVE>
+						<LEDGERNAME>CASH</LEDGERNAME>
+						<AMOUNT>-{row.invoice_amount}</AMOUNT>
+					</LEDGERENTRIES.LIST>
+					<ALLINVENTORYENTRIES.LIST> 
+						<ISDEEMEDPOSITIVE>No</ISDEEMEDPOSITIVE> 
+						<STOCKITEMNAME>{row.sku}</STOCKITEMNAME> 
+						<AMOUNT>{row.tax_exclusive_gross}</AMOUNT> 
+						<ACTUALQTY>{row.quantity}</ACTUALQTY> 
+						<BILLEDQTY>{row.quantity}</BILLEDQTY> 
+						<RATE>{row.tax_exclusive_gross}</RATE> 
+						<ACCOUNTINGALLOCATIONS.LIST> 
+							<ISDEEMEDPOSITIVE>No</ISDEEMEDPOSITIVE> 
+							<LEDGERNAME>Sales</LEDGERNAME> 
+							<AMOUNT>{row.tax_exclusive_gross}</AMOUNT> 
+						</ACCOUNTINGALLOCATIONS.LIST>
+					</ALLINVENTORYENTRIES.LIST>
+					<LEDGERENTRIES.LIST> 
+						<ISDEEMEDPOSITIVE>No</ISDEEMEDPOSITIVE> 
+						<LEDGERNAME>CGST @9%</LEDGERNAME> 
+						<AMOUNT>{row.cgst_tax}</AMOUNT> 
+					</LEDGERENTRIES.LIST> 
+					<LEDGERENTRIES.LIST> 
+						<ISDEEMEDPOSITIVE>No</ISDEEMEDPOSITIVE> 
+						<LEDGERNAME>SGST @9%</LEDGERNAME> 
+						<AMOUNT>{row.sgst_tax}</AMOUNT> 
+					</LEDGERENTRIES.LIST>
+				</VOUCHER>
+			</TALLYMESSAGE>"""
+
+
+		xml_op += """</DATA>
+			</BODY>
+		</ENVELOPE>\n"""
+
+		print(xml_op)
+		return(xml_op)
+		
+
+	def send_tally_request(self, csv_row):
+		xml_row = self.create_xml(csv_row)
+
+		try:
+			response = requests.post(url,data=xml_row, headers=headers)
+		except requetss.exceptions.RequestException as e: #Catch all exceptions
+			return(e)
+
+		if response.status_code == 200:
+			tree = ET.fromstring(response.content)
+			if tree.find("./BODY/DATA/ERRORS").text != '1':
+				if tree.find("./BODY/DATA/CREATED") == '1':
+					return('Created. Voucher ID is:'+ tree.find("./BODY/DATA/LASTVCHID").text +\
+					 'and' + tree.find(tree.find("./BODY/DATA/DESC/CMPINFOEX/IDINFO/LASTCREATEDVCHID").text))
+				else:
+					return('Created Voucher ID not found. No Error.')
+			else:
+				if tree.find("./BODY/DATA/ERRORS") == None:
+					return('Error inserting record.')
+				else:
+					return(tree.find("./BODY/DATA/ERRORS").text)
+		else:
+			return('server error: ', response.status_code)
+
+	def batch_import(self):
+		result = self.df.apply(self.send_tally_request, axis=1) #apply function to each row
+		return(result)
